756e17cced0162364feaf0049a7ef398b92dad0d
Dmitry Lubchenko <d.lubchenko@iconto.net>
2017-04-27 13:47:23 +0300

    Custom keyboard

--- bot.py
+++ bot.py
@@ -2,13 +2,23 @@
 import config
 import telebot
 
+from telebot import types
 
 # Я назову его iEcho_bot
 bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(content_types=["text"])
-def send_message_back(message): 
+def send_message_back(message):
+    # Эхо бота
     bot.send_message(message.chat.id, message.text)
+
+    # Кастомная клава с вариантами
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('🧀 сыр ', ' 🌭 хот-дог ' )
+    markup.row(' 🌯 буррито ', ' 🍕 пицца ')
+    markup.row(' 🌮 тако ' , ' 🍜 рамен' )
+    markup.row(' 🍔 бургер ', ' 🍩 пончик ')
+    bot.send_message(message.chat.id, " Что будешь заказывать? ", reply_markup=markup)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
